ea6548ac8055a0eff628b29ccd90de64e7c7cd6f
Alex Cho <alexkcho97@gmail.com>
2018-10-26 08:13:32 -0400

    Update lab06

--- lab06/index.js
+++ lab06/index.js
@@ -1,7 +1,30 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use('/', express.static(__dirname + '/public'))
+//app.get('/', (req, res) => res.send('Hello World!'))
+
+const writeResponseInBody = (req, res, method) => {
+  res.status(200)
+  res.send('Hello ${method} - ${JSON.stringify(req.body)}')
+}
+
+const writeResponse = (req, res, method) => {
+  res.status(200)
+  res.send('hello ${method}')
+}
+
+
+app.get('/request', (req, res) => writeResponse(req, res, 'GET'))
+app.post('/request', (req, res) => writeResponseInBody(req, res, 'POST'))
+app.put('/request', (req, res) => writeResponseInBody(req, res, 'PUT'))
+app.delete('/request', (req, res) => writeResponse(req, res, 'DELETE'))
+app.head('/request', (req, res) => writeResponse(req, res, 'HEAD'))
+app.all('/request', (req, res) => res.sendStatus(501));
+app.post('/forms', (req, res) => writeResponseInBody(req, res, 'POST'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
